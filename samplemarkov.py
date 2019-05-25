a72c11ba8dcfac5a913ef8565318b30af903b301
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-25 21:55:04 +0200

    pep 8 fixes

--- samplemarkov.py
+++ samplemarkov.py
@@ -1,120 +1,128 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#python 3.6.4
+# python 3.6.4
 
 import random
 import re
 import json
 import numpy
-import sys
+
 
 print("initializing")
 
+
 class word():
-	def __init__(self,initialDict=None,instanceCount=None):
-		if initialDict == None and instanceCount == None:
-			self.followDict = {}
-			self.allInstances = 0
-		else:
-			self.followDict = initialDict
-			self.allInstances = instanceCount
-		return
 
-	def addFollowingWord(self,newWord):
-		try:
-			count = self.followDict[newWord][0]
-			self.allInstances +=1
-			self.followDict[newWord] = (count+1,count/self.allInstances)
-		except KeyError:
-			self.allInstances +=1
-			self.followDict[newWord] = (1,1/self.allInstances)
-		for each in self.followDict.keys():
-			eachCount = self.followDict[each][0]
-			self.followDict[each] = (eachCount,eachCount/self.allInstances)
-		return
+    def __init__(self, initialDict=None, instanceCount=None):
+        if initialDict is None and instanceCount is None:
+            self.followDict = {}
+            self.allInstances = 0
+        else:
+            self.followDict = initialDict
+            self.allInstances = instanceCount
+        return
 
-	def nextWord(self):
-		#sumProb = sum([probability for (count,probability) in self.followDict.values()])
-		#print(sumProb)
-		if len(self.followDict) == 0: #if the dictionary is empty
-			return "."
-		else:
-			return numpy.random.choice(list(self.followDict.keys()),p=[probability for (count,probability) in self.followDict.values()])
+    def addFollowingWord(self, newWord):
+        try:
+            count = self.followDict[newWord][0]
+            self.allInstances += 1
+            self.followDict[newWord] = (count + 1, count / self.allInstances)
+        except KeyError:
+            self.allInstances += 1
+            self.followDict[newWord] = (1, 1 / self.allInstances)
+        for each in self.followDict.keys():
+            eachCount = self.followDict[each][0]
+            self.followDict[each] = (eachCount, eachCount / self.allInstances)
+        return
 
-def prepareJsonWrite(wordDict,initialWords):
-	print("preparing json write")
-	allData = {}
-	allData['initialWords'] = initialWords
-	allData['words'] = {}
-	for key in wordDict.keys():
-		try:
-			test = allData['words'][key]
-		except KeyError:
-			allData['words'][key] = {}
-		allData['words'][key]['nextWords'] = wordDict[key].followDict
-		allData['words'][key]['instanceCount'] = wordDict[key].allInstances
-	print("writing json to file")
-	with open('savedWords.txt','w') as file:
-		json.dump(allData,file)
-	return
+    def nextWord(self):
+        # sumProb = sum([prob for (count,prob) in self.followDict.values()])
+        # print(sumProb)
+        if len(self.followDict) == 0:  # if the dictionary is empty
+            return "."
+        else:
+            probs = [prob for (count, prob) in self.followDict.values()]
+            return numpy.random.choice(list(self.followDict.keys()),
+                                       p=probs)
+
+
+def prepareJsonWrite(wordDict, initialWords):
+    print("preparing json write")
+    allData = {}
+    allData['initialWords'] = initialWords
+    allData['words'] = {}
+    for key in wordDict.keys():
+        try:
+            test = allData['words'][key]
+            print(test)
+        except KeyError:
+            allData['words'][key] = {}
+        allData['words'][key]['nextWords'] = wordDict[key].followDict
+        allData['words'][key]['instanceCount'] = wordDict[key].allInstances
+    print("writing json to file")
+    with open('savedWords.txt', 'w') as file:
+        json.dump(allData, file)
+    return
+
 
 try:
-	print("reading json file")
-	with open('savedWords.txt',encoding='utf-8') as file:
-		jsonData = json.loads(file.read())
+    print("reading json file")
+    with open('savedWords.txt', encoding='utf-8') as file:
+        jsonData = json.loads(file.read())
 
-	initialWords = jsonData['initialWords']
+    initialWords = jsonData['initialWords']
 
-	wordDict = {}
-	for key in jsonData['words'].keys():
-		wordDict[key] = word(initialDict=jsonData['words'][key]['nextWords'],instanceCount=jsonData['words'][key]['instanceCount'])
+    wordDict = {}
+    for key, value in jsonData['words'].items():
+        wordDict[key] = word(initialDict=value['nextWords'],
+                             instanceCount=value['instanceCount'])
 except IOError:
-	print("json file does not exist")
-	wordDict = {}
-	initialWords = []
+    print("json file does not exist")
+    wordDict = {}
+    initialWords = []
 
-	print("reading file")
+    print("reading file")
 
-	with open('sample.txt',encoding='utf-8') as file:
-		text = file.read()
+    with open('sample.txt', encoding='utf-8') as file:
+        text = file.read()
 
-	print("creating word class instances")
+    print("creating word class instances")
 
-	for wordMatch in re.finditer(r'\b[a-zA-Z]+?\b',text,re.IGNORECASE):
-		try:
-			test = wordDict[wordMatch.group(0).lower()]
-		except KeyError:
-			wordDict[wordMatch.group(0).lower()] = word()
+    for wordMatch in re.finditer(r'\b[a-zA-Z]+?\b', text, re.IGNORECASE):
+        try:
+            test = wordDict[wordMatch.group(0).lower()]
+        except KeyError:
+            wordDict[wordMatch.group(0).lower()] = word()
 
-	print("finding sentence start words")
+    print("finding sentence start words")
 
-	for startWord in re.finditer(r'\. \b([a-zA-Z]+?)\b',text):
-		initialWords.append(startWord.group(1))
+    for startWord in re.finditer(r'\. \b([a-zA-Z]+?)\b', text):
+        initialWords.append(startWord.group(1))
 
-	print("finding next word probabilities")
+    print("finding next word probabilities")
 
-	for key in wordDict.keys():
-		for match in re.finditer(r'\b'+key+r'\b ?(\.|[a-zA-Z]+\b)',text,re.IGNORECASE):
-			wordDict[str(key)].addFollowingWord(match.group(1))
+    for key in wordDict.keys():
+        regex_string = r'\b' + key + r'\b ?(\.|[a-zA-Z]+\b)'
+        for match in re.finditer(regex_string, text, re.IGNORECASE):
+            wordDict[str(key)].addFollowingWord(match.group(1))
 
-	prepareJsonWrite(wordDict,initialWords)
+    prepareJsonWrite(wordDict, initialWords)
 
 print("generating markov sentence")
 
 while True:
-	randomWord = random.choice(initialWords)
-	printList = [randomWord]
+    randomWord = random.choice(initialWords)
+    printList = [randomWord]
 
-	while True:
-		previousWord = printList[len(printList)-1].lower()
-		nextWord = wordDict[previousWord].nextWord()
-		if nextWord == ".":
-			break
-		printList.append(nextWord)
+    while True:
+        previousWord = printList[len(printList) - 1].lower()
+        nextWord = wordDict[previousWord].nextWord()
+        if nextWord == ".":
+            break
+        printList.append(nextWord)
 
-	print("markov sentence:\n")
-	print(" ".join(printList)+".")
-	choice = input("\ngenerate more? y/n\n")
-	if choice == 'n':
-		break
-sys.exit()
+    print("markov sentence:\n")
+    print(" ".join(printList) + ".")
+    choice = input("\ngenerate more? y/n\n")
+    if choice == 'n':
+        break
